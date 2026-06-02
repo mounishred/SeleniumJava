4b44cb70447c9d26d2e5854e9491a07363bec11f
mounishred <mounish682@gmail.com>
2026-06-02 17:41:25 +0530

    Github code added and reports

--- src/selenium/GitHub.java
+++ src/selenium/GitHub.java
@@ -1,14 +1,22 @@
 package selenium;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class GitHub {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void google() {
+	  driver.get("https://www.google.com");
+	  driver.findElement(By.name("q")).sendKeys("Selenium with GitHub");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
