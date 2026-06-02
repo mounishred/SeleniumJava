f1e7c8f01c99b1a6e8dfa72325ad9474ee7483cd
mounishred <mounish682@gmail.com>
2026-06-02 17:49:29 +0530

    Update Navigations x replaced with google and thread.sleep removed.java

--- src/navigationmethods/Navigations.java
+++ src/navigationmethods/Navigations.java
@@ -9,14 +9,13 @@ public class Navigations {
 
 			WebDriver driver = new ChromeDriver();
 			driver.manage().window().maximize();
-			driver.get("https://www.google.com");
-			Thread.sleep(2000);
+			driver.get("https://www.x.com");
 			driver.navigate().to("https://www.facebook.com");
-			Thread.sleep(2000);
+			
 			driver.navigate().back();
-			Thread.sleep(2000);
+			
 			driver.navigate().forward();
-			Thread.sleep(2000);
+		
 			driver.navigate().refresh();
 	}
 
