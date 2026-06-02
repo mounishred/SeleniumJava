6ad717cf3f5fa10dbbdffd2cb74cc589714ff406
mounishred <mounish682@gmail.com>
2026-06-02 17:47:55 +0530

    Update JavaComments by Purushotham.java

--- src/javacomments/JavaComments.java
+++ src/javacomments/JavaComments.java
@@ -4,10 +4,10 @@ public class JavaComments {
 	
 	public static void main(String[] args) {
 	
-System.out.println("Hello");
+System.out.println("Purushotham");
 //System.out.println("#$%#@%#$^%^&^%*SDGFFHGJ34546457");
-System.out.println("Selenium");
-/* System.out.println('5');
+System.out.println("GitHub");
+/* System.out.println('8');
 System.out.println('K');
 System.out.println('9');
 System.out.println(8); */
