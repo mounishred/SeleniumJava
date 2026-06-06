356f66c411245824f5438fdac7a895dea90d7179
mounishred <mounish682@gmail.com>
2026-06-06 13:17:34 +0530

    Some changes had made & sorted.

---
+++ src/screenshots/timeFormatScreenshot.java
@@ -0,0 +1,46 @@
+package screenshots;
+
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class timeFormatScreenshot {
+	
+	WebDriver driver;
+	
+	public void Screenshot() throws IOException{
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyy_MMM_dd hh_mm_ss");
+		Date date = new Date(0);
+		String time = dateFormat.format(date);
+		
+		File f = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(f,new File("/Users/mounishreddyparlapalli/eclipse-workspace/SeleniumJava/Screenshots" + time +".png"));
+		
+	}
+	
+  @Test
+  public void f()  throws Exception{
+	  
+	  driver.get("https://www.flipkart.com");
+	  Thread.sleep(2000);
+	  Screenshot();
+	  
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+  }
+
+}
---
+++ src/trycatch/TryCatchBlock.java
@@ -0,0 +1,24 @@
+package trycatch;
+
+public class TryCatchBlock {
+
+	public static void main(String[] args) {
+		//int num[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		//System.out.println(num[8]);
+		
+		try
+		{
+			int num[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+			System.out.println(num[4]);
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("Your'e accessing wrong index no");
+			System.out.println("==== Error due to exception is:"+e);
+		}
+		
+
+	}
+
+}
